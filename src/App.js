79ebec81663589b73559c5b355595c443c717d73
Vineeth Maller <vineethmaller@gmail.com>
2021-01-01 15:31:39 +0530

    Setup base files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './resources/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDuPVpodoIh6B_cuGKOz4LwsvyiZQMXBKA",
+  authDomain: "qidag-app.firebaseapp.com",
+  projectId: "qidag-app",
+  storageBucket: "qidag-app.appspot.com",
+  messagingSenderId: "957382795517",
+  appId: "1:957382795517:web:bfbd8e390bdf21ca89de94",
+  measurementId: "G-S3SV5QY7G6"
+};
